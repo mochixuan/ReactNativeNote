112778578bf290f8ccc741ddbc02a432fcef1130
莫辞旋 <1306894924@qq.com>
2017-06-20 21:24:32 +0800

    second

--- StudyFollow/BootStrapGold/js/main.js
+++ StudyFollow/BootStrapGold/js/main.js
@@ -1,1 +1,20 @@
+'use strict';
 
+$(function () {
+    function resize() {
+        var windowWidth = $(window).width();
+        var isSmallScreen = windowWidth < 768;
+        $('#main_ad > .carousel-inner .item').each(function (i,item) {
+            console.log(333);
+            var $item = $(item);
+            var imgSrc = isSmallScreen ? $item.data('image-xs') : $(item).data('image-lg');
+            $item.css('backgroundImage','url("'+imgSrc+'")');
+            if (isSmallScreen) {
+                $item.html('<img src="' + imgSrc + '" alt="" />');
+            } else {
+                $item.empty();
+            }
+        });
+    }
+    $(window).on('resize',resize).trigger('resize');
+});
