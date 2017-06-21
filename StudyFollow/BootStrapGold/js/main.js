bb842f9914b71d6d99cb65c8711b987ab641e45d
莫辞旋 <1306894924@qq.com>
2017-06-21 20:44:01 +0800

    ku

--- StudyFollow/BootStrapGold/js/main.js
+++ StudyFollow/BootStrapGold/js/main.js
@@ -5,7 +5,6 @@ $(function () {
         var windowWidth = $(window).width();
         var isSmallScreen = windowWidth < 768;
         $('#main_ad > .carousel-inner .item').each(function (i,item) {
-            console.log(333);
             var $item = $(item);
             var imgSrc = isSmallScreen ? $item.data('image-xs') : $(item).data('image-lg');
             $item.css('backgroundImage','url("'+imgSrc+'")');
