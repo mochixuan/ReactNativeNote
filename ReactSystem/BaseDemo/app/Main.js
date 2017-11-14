e6541266d85f40d3290fc15a1e77aba4d6e82d39
wang <1306894924@qq.com>
2017-11-14 19:53:06 +0800

    Medicine can not stop

--- ReactSystem/BaseDemo/app/Main.js
+++ ReactSystem/BaseDemo/app/Main.js
@@ -1,17 +1,65 @@
 import React,{Component} from 'react'
 import {
+    StyleSheet,
     View,
-    Text
+    Text,
+    TouchableHighlight,
+    Platform,
+    ToastAndroid,
 } from 'react-native'
 
 export default class Main extends Component {
 
     render() {
+
+        const itemView = [
+            this._renderButtonView("Main",()=>{this.show("单机")})
+        ]
+
         return (
-            <View>
-                <Text>Hello</Text>
+            <View style={styles.container}>
+                {
+                    itemView.map((item,i)=>{
+                        return <View key={i}>{item}</View>
+                    })
+                }
             </View>
         )
     }
 
+    _renderButtonView(text,onPress) {
+        return (
+            <TouchableHighlight
+                underlayColor="rgba(77,94,225,0.5)"
+                onPress={()=>onPress()}
+                style={styles.button_view}>
+                <Text style={styles.button_text}>{text}</Text>
+            </TouchableHighlight>
+        )
+    }
+
+    show(data) {
+        ToastAndroid.show(data,ToastAndroid.SHORT)
+    }
+
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        margin: 8,
+    },
+    button_view: {
+        padding: 6,
+        margin: 4,
+        backgroundColor: '#5fc0a5',
+        alignItems: 'center',
+        borderRadius:4,
+    },
+    button_text: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#f0f0f0',
+    }
+})
---
+++ ReactSystem/BaseMaster1/App.js
@@ -0,0 +1,21 @@
+import {StackNavigator} from 'react-navigation'
+import CardStackStyleInterpolator from 'react-navigation/src/views/CardStack/CardStackStyleInterpolator'
+import Main from "./app/Main";
+
+const App = StackNavigator({
+    Main: {screen: Main}
+},{
+  navigationOptions: {
+    gesturesEnabled: true,
+  },
+    headerMode: "none",
+    transitionConfig:(()=>({
+        //暂时不支持
+        //screenInterpolator: CardStackStyleInterpolator.forHorizontal()
+    }))
+})
+
+export default App
+
+
+
